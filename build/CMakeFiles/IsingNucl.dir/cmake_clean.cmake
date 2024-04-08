6807f06b8fee0c10a08716ec75996b8fa5d0cc89
AdityaDendukuri <dendukuri.aditya123@outlook.com>
2024-04-07 23:23:31 -0700

    new edits

---
+++ build/CMakeFiles/IsingNucl.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IsingNucl.dir/main.c.o"
+  "CMakeFiles/IsingNucl.dir/main.c.o.d"
+  "IsingNucl"
+  "IsingNucl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/IsingNucl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/IsingNucl.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for IsingNucl.
+# This may be replaced when dependencies are built.
